20dcb582b687e9a0be1eeda156d1406b523c6fa0
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2022-12-07 14:53:06 -0800

    added global variable of all posible combinations

--- Project1/Project1/tab.cpp
+++ Project1/Project1/tab.cpp
@@ -1,5 +1,7 @@
 #include "tab.h"
 #include <string>
+#include <vector>
 
 std::string global_tablero;
 std::string global_tablero3;
+std::vector<std::string> allWordsInBoard;
--- Project1/Project1/tab.h
+++ Project1/Project1/tab.h
@@ -1,4 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 extern std::string global_tablero;
 extern std::string global_tablero3;
+extern std::vector<std::string> allWordsInBoard;
